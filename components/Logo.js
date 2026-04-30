a97285de7e6d2984692b46dcb7e618071e176e2f
carlosnachon-eng <carlos.nachon@emporioinmobiliario.mx>
2026-04-29 19:51:41 -0600

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -2,7 +2,7 @@ export default function Logo({ size = 40, dark = false }) {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <img
-        src="/IMG_7034.jpeg"
+        src="/logo.png"
         alt="Emporio Inmobiliario"
         style={{ height: size * 1.4, width: "auto", objectFit: "contain" }}
       />
